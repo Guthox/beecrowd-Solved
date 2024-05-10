8b55ec574b5efb544e4169f7f8c783c719418bc9
Guthox <gutho.san@gmail.com>
2024-05-10 16:51:42 -0300

    Adicionado 3299, 3302 e 3303

---
+++ Python/3299.py
@@ -0,0 +1,1 @@
+print(i + " NO es de Mala Suerte") if (i := input()).find("13") == -1 else print(i + " es de Mala Suerte")
---
+++ Python/3302.py
@@ -0,0 +1,1 @@
+[print(f"resposta {i+1}: {int(input())}") for i in range(int(input()))]
